25ba65910b916ed25ff7afef7e18b8a72e439203
Zafir <zafirskthelifehacker@hotmail.com>
2024-07-13 23:02:44 +0400

    Cleanup

--- tests/integration/input-modal-test.ts
+++ tests/integration/input-modal-test.ts
@@ -1,17 +1,18 @@
 // tests/integration/components/input-modal-test.ts
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { pauseTest, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | input-modal', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders input fields and a save button', async function (assert) {
+  test('it renders input fields, a close button and a save button', async function (assert) {
     await render(hbs`<InputModal />`);
     assert.dom('input[type="text"]:nth-of-type(1)').exists();
     assert.dom('input[type="text"]:nth-of-type(2)').exists();
     assert.dom('input[type="email"]').exists();
+    await pauseTest();
     assert.dom('button:nth-of-type(1)').hasText('X');
     assert.dom('button:nth-of-type(2)').hasText('Save');
   });
