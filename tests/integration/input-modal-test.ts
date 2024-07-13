ce1541796dfd078a6b6d03bf0aa98f053f70c93e
Zafir <zafirskthelifehacker@hotmail.com>
2024-07-13 22:50:42 +0400

    Fix lint

--- tests/integration/input-modal-test.ts
+++ tests/integration/input-modal-test.ts
@@ -1,14 +1,14 @@
 // tests/integration/components/input-modal-test.ts
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, fillIn, click } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | input-modal', function(hooks) {
+module('Integration | Component | input-modal', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders input fields and a save button', async function(assert) {
-    await render(hbs`<InputModal @closeModal={{action (mut this.isModalOpen) false}} />`);
+  test('it renders input fields and a save button', async function (assert) {
+    await render(hbs`<InputModal />`);
     assert.dom('input[type="text"]:nth-of-type(1)').exists();
     assert.dom('input[type="text"]:nth-of-type(2)').exists();
     assert.dom('input[type="email"]').exists();
