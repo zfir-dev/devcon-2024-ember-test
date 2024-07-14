b0c1adcbeedf27c1e83e01166542c9bd101354ba
Keshav Caleechurn <keshav@Keshavs-MacBook-Pro.local>
2024-07-14 16:25:31 +0400

    Add UserInfo component with tests

--- app/components/user-info.ts
+++ app/components/user-info.ts
@@ -1,18 +1,21 @@
-import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class UserInfoComponent extends Component {
     @tracked name = '';
+    @tracked errorMsg = '';
 
     constructor(a: never, b: never) {
         super(a, b);
+
+        window.addEventListener('message', (e) => {
+            if (e.data.name === 'error-message') this.errorMsg = e.data.message;
+        })
+        // window.parent.postMessage({ name: 'error-message', message: 'unknown user !' });
         this.setName();
     }
 
-    @action 
-    async setName(): Promise<void> {
-        setTimeout(  
-        () => this.name = 'John', 2000);
+    setName(): void {
+        this.name = 'John';
     }
 }
