966c359e5f6799347a3bb476f4469c5b89a7ceaa
Zafir <zafirskthelifehacker@hotmail.com>
2024-07-14 23:23:20 +0400

    Excalidraw

---
+++ tests/integration/components/excalidraw-test.ts
@@ -0,0 +1,25 @@
+// tests/integration/components/input-modal-test.ts
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, find, render, settled, triggerKeyEvent, waitFor, waitUntil } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | excalidraw', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders iframe with excalidraw', async function (assert) {
+    await render(hbs`<Excalidraw />`);
+    await settled();
+    assert.dom('iframe').exists();
+    assert.dom('iframe').hasAttribute('src', 'https://excalidraw.com');
+
+  });
+
+  test('it renders an iframe and selects rectangle tool', async function (assert) {
+    await render(hbs`<Excalidraw />`);
+
+    const iframe = find('iframe') as HTMLIFrameElement;
+
+
+  });
+});
